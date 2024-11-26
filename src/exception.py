d4dfe7a846533b61c4ceb7bf01ed9ec8ef3dbc97
rbind403@gmail.com <rbind403@gmail.com>
2024-11-26 14:31:59 +0530

    adding data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,14 +1,24 @@
 import sys
+from src.logger import logging
 
+class CustomException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        _, _, exc_tb = error_details.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        self.error_message = (
+            f"Error occurred in python script name [{file_name}] "
+            f"line number [{line_number}] error message [{error_message}]"
+        )
+        super().__init__(self.error_message)
 
-class customexception(Exception):
-    def __init__(self,error_message,error_details:sys):
-        self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
-        
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename 
-    
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
+        return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info('An error occurred: dividing by zero.')
+        raise CustomException(str(e), sys)
--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import os
 import logging
 from datetime import datetime 
 
-LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H')}.log"
 
 log_path = os.path.join(os.getcwd(), "logs")
 
